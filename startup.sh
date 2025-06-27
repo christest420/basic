dde68a52ee7b2f5d86a1392f9455ce9b18800c3a
christest420 <christopher@fossplater.no>
2025-06-27 09:10:56 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "🚀 Starter installasjon av dependencies..."
-pip install -r requirements.txt
+echo "🚀 Starter installasjon av dependencies..." >&2
+pip install -r requirements.txt >&2
 
-echo "✅ Dependencies installert, starter app..."
+echo "✅ Dependencies installert, starter app..." >&2
 gunicorn --bind=0.0.0.0:8000 api:app
